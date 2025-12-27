d598697923f04a5e3c21a047006a01f6e494bf47
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-12-27 19:21:38 +0100

    skip tests on osx arm64 and add build dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,7 @@ set -ex
 
 cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -G Ninja -S . -B build
 cmake --build build
-cmake --build build --target check_re2c
+if [[ "$HOST" != arm64-apple* ]]; then
+    cmake --build build --target check_re2c
+fi
 cmake --build build --target install
